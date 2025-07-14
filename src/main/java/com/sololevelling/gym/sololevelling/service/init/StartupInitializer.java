936bb3442ae0804baf9228eb9442633e20a33fbc
PraveenKumar <159308991+unique6246@users.noreply.github.com>
2025-07-15 02:10:56 +0530

    add license information to multiple Java files

--- src/main/java/com/sololevelling/gym/sololevelling/service/init/StartupInitializer.java
+++ src/main/java/com/sololevelling/gym/sololevelling/service/init/StartupInitializer.java
@@ -8,16 +8,6 @@
 
  */
 
-/*
-
-  * © 2025 Praveen Kumar. All rights reserved.
-  *
-  * This software is licensed under the MIT License.
-  * See the LICENSE file in the root directory for more information.
-
-
- */
-
 package com.sololevelling.gym.sololevelling.service.init;
 
 import com.sololevelling.gym.sololevelling.model.Role;
--- src/main/java/com/sololevelling/gym/sololevelling/util/DungeonNotFoundException.java
+++ src/main/java/com/sololevelling/gym/sololevelling/util/DungeonNotFoundException.java
@@ -8,15 +8,6 @@
 
  */
 
-/*
-
- * © 2025 Praveen Kumar. All rights reserved.
- *
- * This software is licensed under the MIT License.
- * See the LICENSE file in the root directory for more information.
-
-
- */
 
 package com.sololevelling.gym.sololevelling.util;
 
